5445be66b3638c86444285b63e54d9f67456be4d
Pankaj Ramesh Vasnani <pankaj.techuser@gmail.com>
2021-03-26 10:01:48 +0700

    Make API for food menu access

---
+++ app/app/Http/Controllers/MenuController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class MenuController extends Controller
+{
+    /**
+     * Function to get the food menu
+     * 
+     * @param $request as input data
+     * 
+     * @return food menu data
+     */
+    public function index(Request $request) {
+        $data = Item::where('status', config('constants.item_status.active'))->get();
+
+        return interpretJsonResponse(true, 200, $data, null);
+    }
+}
--- app/resources/views/layouts/master.blade.php
+++ app/resources/views/layouts/master.blade.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>App Name - @yield('title')</title>
+        <title>Food Ordering System</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
